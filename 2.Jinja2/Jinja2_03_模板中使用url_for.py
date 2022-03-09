1e3e676f2fb37e8fe0b972f4778fa6c0d985b19c
lina0939356997 <lina09393569997@gmail.com>
2022-03-09 15:33:12 +0800

    update

---
+++ 2.Jinja2/Jinja2_03_模板中使用url_for.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index_03.html')
+
+# {{用來存放變量}}
+# {% 用來執行函數或邏輯代碼 %}
+@app.route('/acc/login/<id>/')
+def login():
+    return render_template('index_03_login.html')
+
+
+if __name__ == '__main__':
+    app.run()
