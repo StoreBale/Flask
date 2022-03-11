c4fac03a25dd33f1bb65270fef423db4e0d3f8df
lina0939356997 <lina09393569997@gmail.com>
2022-03-11 11:31:05 +0800

    new FlaskH_05

--- 3.Flask高級視圖/FlaskH_05/news.py
+++ 3.Flask高級視圖/FlaskH_05/news.py
@@ -1,13 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-news_bp = Blueprint('news', __name__)
+news_bp = Blueprint('news', __name__, url_prefix='/news', template_folder='test')
+
 
 @news_bp.route('/list/')
 def news_list():
-    return '新聞列表'
+    return render_template('test.html')
+
 
 @news_bp.route('/detail/')
 def news_detail():
     return '新聞詳情'
-
-
