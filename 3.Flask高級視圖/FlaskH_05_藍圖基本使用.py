c3266012da9066a07c4fef32010c227cfc4ad4c3
lina0939356997 <lina09393569997@gmail.com>
2022-03-11 13:51:59 +0800

    update

--- 3.Flask高級視圖/FlaskH_05_藍圖基本使用.py
+++ 3.Flask高級視圖/FlaskH_05_藍圖基本使用.py
@@ -1,10 +1,13 @@
-from flask import Flask, url_for,render_template
+from flask import Flask, url_for, render_template
 from FlaskH_05.user import user_bp
 from FlaskH_05.news import news_bp
+from FlaskH_05.cms import cms_bp
 
 app = Flask(__name__)
+app.config['SERVER_NAME'] = 'test.com:5000'
 app.register_blueprint(user_bp)
 app.register_blueprint(news_bp)
+app.register_blueprint(cms_bp)
 
 
 @app.route('/')
