46342ca9d47f9a78b9c914461f1a6637accfc143
lina0939356997 <lina09393569997@gmail.com>
2022-03-11 17:38:36 +0800

    new FlaskB_02

---
+++ 4.Flask數據庫/FlaskB_02_SQLAlchemy.py
@@ -0,0 +1,21 @@
+# coding = utf-8
+from sqlalchemy import create_engine
+
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+# 進入之前資料庫一定要先創建完成
+DATABASE = 'first_sqlalchemy'
+USERNAME = 'root'
+PASSWORD = '929526'
+
+DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8".format(username=USERNAME,
+                                                                                        password=PASSWORD,
+                                                                                        host=HOSTNAME,
+                                                                                        port=PORT,
+                                                                                        db=DATABASE)
+engine = create_engine(DB_URI)
+
+# 判斷是否連接成功
+conn = engine.connect()
+result = conn.execute('select 1')
+print(result.fetchone())
