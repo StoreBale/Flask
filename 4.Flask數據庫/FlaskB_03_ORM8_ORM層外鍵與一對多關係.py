c1765052afc74a2f9e6701e65085614ab5980fcc
lina0939356997 <lina09393569997@gmail.com>
2022-03-15 15:05:43 +0800

    new FlaskB_03_ORM

---
+++ 4.Flask數據庫/FlaskB_03_ORM8_ORM層外鍵與一對多關係.py
@@ -0,0 +1,54 @@
+#encoding: utf-8
+
+from sqlalchemy import create_engine,Column,Integer,Float,Boolean,DECIMAL,Enum,Date,DateTime,Time,String,Text,func,and_,or_,ForeignKey
+from sqlalchemy.dialects.mysql import LONGTEXT
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker,relationship
+# 在Python3中才有这个enum模块，在python2中没有
+import enum
+from datetime import datetime
+import random
+
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'first_sqlalchemy'
+USERNAME = 'root'
+PASSWORD = '929526'
+
+# dialect+driver://username:password@host:port/database
+DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8mb4".format(username=USERNAME,password=PASSWORD,host=HOSTNAME,port=PORT,db=DATABASE)
+
+engine = create_engine(DB_URI)
+
+Base = declarative_base(engine)
+
+session = sessionmaker(engine)()
+
+# 父表 / 从表
+# user/article
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    username = Column(String(50),nullable=False)
+
+    # articles = relationship("Article")
+
+    def __repr__(self):
+        return f"<Article (id:{self.id} username:{self.username})>"
+
+
+class Article(Base):
+    __tablename__ = 'article'
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    title = Column(String(50),nullable=False)
+    content = Column(Text,nullable=False)
+    uid = Column(Integer,ForeignKey("user.id"))
+
+    author = relationship("User",backref='articles')
+
+    def __repr__(self):
+        return f"<Article (id:{self.id} title:{self.title} content:{self.content} uid:{self.uid})>"
+
+user = session.query(User).first()
+print(user.articles)
---
+++ 4.Flask數據庫/FlaskB_03_ORM99OX_ORM層面刪除數據.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'first_sqlalchemy'
+USERNAME = 'root'
+PASSWORD = '929526'
+
+# dialect+driver://username:password@host:port/database
+DB_URI = "mysql+pymysql://{username}:{password}@{host}:{port}/{db}?charset=utf8".format(username=USERNAME,
+                                                                                        password=PASSWORD,
+                                                                                        host=HOSTNAME, port=PORT,
+                                                                                        db=DATABASE)
+
+engine = create_engine(DB_URI)
+Base = declarative_base(engine)
+session = sessionmaker(engine)()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), nullable=False)
+
+
+class Article(Base):
+    __tablename__ = 'article'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(50), nullable=False)
+    uid = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+    author = relationship("User", backref='article')
+
+
+# Base.metadata.drop_all()
+# Base.metadata.create_all()
+#
+# user = User(username='Zscing')
+# article = Article(title="hey")
+# article.author = user
+# session.add(article)
+# session.commit()
+
+user = session.query(User).first()
+session.delete(user)
+session.commit()
+
