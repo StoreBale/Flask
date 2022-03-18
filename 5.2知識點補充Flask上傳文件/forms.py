845908f80ce2fe7be1f137577bce84220c0a9e7e
lina0939356997 <lina09393569997@gmail.com>
2022-03-18 09:05:15 +0800

    update Folder name

---
+++ 5.2知識點補充Flask上傳文件/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf.file import FileRequired, FileAllowed
+from wtforms import Form, FileField, StringField
+from wtforms.validators import InputRequired
+
+
+class UploadForm(Form):
+    avatar = FileField(validators=[FileRequired(), FileAllowed(['jpg', 'png', 'gif'])])
+    desc = StringField(validators=[InputRequired()])
